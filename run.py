c3a0a54c06eaffab4340b6d214191393d71455a1
Toan Le <82274052+TimoWoodner@users.noreply.github.com>
2022-08-25 15:07:03 +0700

    update run

--- run.py
+++ run.py
@@ -3,7 +3,23 @@
 
 """Convenience wrapper for running GoogleScrapper directly from source tree."""
 
-from GoogleScraper.core import main
+from GoogleScraper import scrape_with_config
+
+def main():
+    config = {
+            'keyword': 'Kenichi Sato Caddi',
+            'search_engines': ['Google'],
+            'num_pages_for_keyword': 1,
+            'output_filename': "csv_test.csv",
+            'scrape_method': 'selenium',
+            'sel_browser': 'chrome',
+            'do_caching': False,
+            'num_workers': 5
+        }
+
+    search = scrape_with_config(config)
+    for serp in search.serps:
+        print(serp)
 
 if __name__ == '__main__':
     main()
